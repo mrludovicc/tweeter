52cebf693614ddbda37529e01f685608ca21afe9
mrludovicc <nadal46@hotmail.com>
2023-03-02 18:11:28 -0500

    Eslint the files

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,40 +1,11 @@
-/*
- * Client-side JS logic goes here
- * jQuery is already loaded
- * Reminder: Use (and do all your DOM work in) jQuery's document ready function
- */
-
-const tweets = [
-  {
-    "user": {
-      "name": "Newton",
-      "avatars": "https://i.imgur.com/73hZDYK.png",
-      "handle": "@SirIsaac"
-    },
-    "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-    "created_at": 1677519565757
-  },
-  {
-    "user": {
-      "name": "Descartes",
-      "avatars": "https://i.imgur.com/nlhLi3I.png",
-      "handle": "@rd"
-    },
-    "content": {
-      "text": "Je pense , donc je suis"
-    },
-    "created_at": 1677605965757
-  }
-]
-
-const escapeP = function (str) {
+//--------- ****** ******* ---------//
+const escapeP = function(str) {
   let div = document.createElement("div");
   div.appendChild(document.createTextNode(str));
   return div.innerHTML;
 };
 
+//--------- ****** ******* ---------//
 const createTweetElement = (tweet) => {
   const safeHTML = escapeP(tweet.content.text);
   const tweetUser = tweet.user;
@@ -61,46 +32,51 @@ const createTweetElement = (tweet) => {
         </div>
       </footer>
     </section>
-  `
+  `;
   return $tweet;
-}
+};
 
-
+//--------- ****** ******* ---------//
 const renderTweets = (tweets) => {
   for (const user of tweets) {
-    const $tweet = createTweetElement(user)
-    $(`.tweets-container`).prepend($tweet)
+    const $tweet = createTweetElement(user);
+    $(`.tweets-container`).prepend($tweet);
   }
-}
+};
 
+//--------- ****** ******* ---------//
 const submitTweets = () => {
-  $("#submit-form").submit(function (e) {
+  $("#submit-form").submit(function(e) {
     e.preventDefault();
     if ($('#tweet-text').val().length === 0) {
-      return $('#empty-text').slideDown('slow').delay(2200).fadeOut('slow')
+      return $('#empty-text').slideDown('slow').delay(2200).fadeOut('slow');
     }
     if ($('#tweet-text').val().length > 140) {
-      return $('#long-text').slideDown('slow').delay(2200).fadeOut('slow')
+      return $('#long-text').slideDown('slow').delay(2200).fadeOut('slow');
     }
     $.post("/tweets", $("#submit-form").serialize())
       .then(() => {
-        $('#tweet-text').val('')
-        $('.counter').val(140)
-        loadTweets()
-      })
+        $('#tweet-text').val('');
+        $('.counter').val(140);
+        loadTweets();
+      });
 
   });
-}
+};
 
+//--------- ****** ******* ---------//
 const loadTweets = () => {
   $.ajax('/tweets', { method: 'GET' })
     .then((data) => {
       renderTweets(data);
-    })
-}
+    });
+};
 
+//--------- ****** ******* ---------//
 const scrollUp = () => {
-  window.onscroll = function () { scrollFunction() };
+  window.onscroll = function() {
+    scrollFunction();
+  };
 
   function scrollFunction() {
     if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
@@ -110,27 +86,28 @@ const scrollUp = () => {
     }
   }
 
-  document.querySelector('.scroll-to-top').addEventListener('click', function () {
+  document.querySelector('.scroll-to-top').addEventListener('click', function() {
     window.scrollTo({ top: 0, behavior: 'smooth' });
   });
-}
+};
 
+//--------- ****** ******* ---------//
 const redirecting = () => {
   const jumpToTextarea = document.querySelector('#jump-to-textarea');
   const jumpToTextarea2 = document.querySelector('#jump-to-textarea2');
   const textarea = document.querySelector('#jump-here');
-  jumpToTextarea.addEventListener('click', function () {
+  jumpToTextarea.addEventListener('click', function() {
     textarea.scrollIntoView({ behavior: 'smooth' });
   });
-  jumpToTextarea2.addEventListener('click', function () {
+  jumpToTextarea2.addEventListener('click', function() {
     textarea.scrollIntoView({ behavior: 'smooth' });
   });
-}
+};
 
+//--------- RUNS THE WEBSITE ---------//
 $(() => {
-
   scrollUp();
   redirecting();
   submitTweets();
   loadTweets();
-})
+});
